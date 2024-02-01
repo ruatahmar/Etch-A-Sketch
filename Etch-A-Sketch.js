5b3fa67dd6fc4ffb8971462e50058d62380cd8bc
ruatahmar <122161393+ruatahmar@users.noreply.github.com>
2024-02-02 00:38:47 +0530

    hover functionality added

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -7,12 +7,24 @@ for(let i=1;i<=16;i++){
     for(let j=1;j<=16;j++){
         
         const deepDiv=document.createElement("div");
-        deepDiv.textContent=i+" "+j;
+        //deepDiv.textContent=i+" "+j;
         deepDiv.setAttribute("class","deepDiv")
         
         insideDiv.appendChild(deepDiv);
     }
     
 }
+const allInsideDiv=document.querySelectorAll(".deepDiv");
+allInsideDiv.forEach((div)=>{
+    div.addEventListener("mouseover",()=>{
+        div.classList.add("colored");
+    })
+});
 
-//
+
+/*
+allInsideDiv.addEventListener("click",()=>{
+    allInsideDiv.classList.add("colored");
+    console.log("touched");
+});
+*/
