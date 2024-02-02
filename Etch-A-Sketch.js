363765f9b959e47e4797e9a7048af22c618a3f01
ruatahmar <122161393+ruatahmar@users.noreply.github.com>
2024-02-02 09:47:50 +0530

    change grid option

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,10 +1,21 @@
-const mainDiv=document.querySelector("div");
+const input=document.querySelector("input");
+const gridButton=document.querySelector("#gridButton");
+gridButton.addEventListener('click',()=>{
+    removeGrid();
+    size=input.value;
+    input.value='';
+    makeGrid();
+    coloredGrid();
+});
 
-for(let i=1;i<=16;i++){
+const mainDiv=document.querySelector("#mainDiv");
+let size=16;
+function makeGrid(){
+for(let i=1;i<=size;i++){
     const insideDiv=document.createElement("div");
     insideDiv.setAttribute("class","insideDiv");
     mainDiv.appendChild(insideDiv);
-    for(let j=1;j<=16;j++){
+    for(let j=1;j<=size;j++){
         
         const deepDiv=document.createElement("div");
         //deepDiv.textContent=i+" "+j;
@@ -12,13 +23,22 @@ for(let i=1;i<=16;i++){
         
         insideDiv.appendChild(deepDiv);
     }
-    
 }
+}
+function removeGrid(){
+    const insideDiv=document.querySelectorAll(".insideDiv");
+    insideDiv.forEach((div)=>{
+        mainDiv.removeChild(div);
+    });
+}
+makeGrid();
+coloredGrid();
 var RGBColor1;
 function random_rgba() {
     var o = Math.round, r = Math.random, s = 255;
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
 }
+function coloredGrid(){
 const allInsideDiv=document.querySelectorAll(".deepDiv");
 allInsideDiv.forEach((div)=>{
     div.addEventListener("mouseover",()=>{
@@ -28,8 +48,7 @@ allInsideDiv.forEach((div)=>{
         //div.setAttribute("style","background:#RGBColor1;")
     })
 });
-
-
+}
 /*
 allInsideDiv.addEventListener("click",()=>{
     allInsideDiv.classList.add("colored");
